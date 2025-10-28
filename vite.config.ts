4a74fdeba1216509171de778b3dedccbcc19b66d
mayankatci <mayank@coreinnovate.co>
2025-10-28 23:02:13 +0530

    fix: fixing the path for the deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/' : '/'},
+  base: mode === 'production' ? '/mayanktrivedi.github.io/' : '/',
   server: {
     host: "::",
     port: 8080,
